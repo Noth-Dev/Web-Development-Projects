7c16482ad762a019e0a87a205e51de1d367ec816
Noth-Dev <103224207+Noth-Dev@users.noreply.github.com>
2022-06-17 11:58:36 +0200

    Completed dashboard

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-import './index.css'
-import App from './App';
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <DarkModeContextProvider>
+      <App />
+    </DarkModeContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
