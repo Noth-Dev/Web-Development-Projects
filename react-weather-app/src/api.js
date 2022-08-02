82b1d071b6257f1711d2ace24af14efeaaab7671
Noth-Dev <103224207+Noth-Dev@users.noreply.github.com>
2022-08-02 15:54:34 +0200

    New Project

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cfa449ba99msh6e3deca427312c1p191d03jsnb31a936c8732",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "77836bdfb3005f5edb0a41755008c845"; // enter your key from openweather API
