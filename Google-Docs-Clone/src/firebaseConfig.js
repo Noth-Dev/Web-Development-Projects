56e454b8245645b537e427e6cdcfc5c3815f9edb
Noth-Dev <103224207+Noth-Dev@users.noreply.github.com>
2022-08-02 15:40:43 +0200

    Docs Clone
    Firebase, React, MateriaUI

--- Google-Docs-Clone/src/firebaseConfig.js
+++ Google-Docs-Clone/src/firebaseConfig.js
@@ -3,7 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  //Your config data
+  apiKey: "AIzaSyC7_jo9Nkt4lOlTrws_LDB0u-7D0yBKi4I",
+  authDomain: "docsclone-a70db.firebaseapp.com",
+  projectId: "docsclone-a70db",
+  storageBucket: "docsclone-a70db.appspot.com",
+  messagingSenderId: "431983253189",
+  appId: "1:431983253189:web:43e61a51e5c1bd0faa02ac"
 };
 
 export const app = initializeApp(firebaseConfig);
